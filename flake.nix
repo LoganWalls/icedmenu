5edb6a625e6d47d6146aaf9cf2fc213206513b88
Logan Walls <2934282+LoganWalls@users.noreply.github.com>
2023-04-18 09:50:26 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "A cross-platform dmenu, iced";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs = {
+        flake-utils.follows = "flake-utils";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    crane,
+    fenix,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+        inherit (pkgs) lib stdenv;
+        fenixPkgs = fenix.packages.${system};
+        rustToolchain = fenixPkgs.default.toolchain;
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+        buildDeps =
+          []
+          ++ lib.optionals stdenv.isDarwin (with pkgs.darwin.apple_sdk.frameworks; [
+            QuartzCore
+            AppKit
+            pkgs.libiconv
+          ]);
+
+        crate = craneLib.buildPackage {
+          src = craneLib.cleanCargoSource ./.;
+          buildInputs = buildDeps;
+        };
+      in {
+        checks = {
+          inherit crate;
+        };
+        packages.default = crate;
+        apps.default = flake-utils.lib.mkApp {
+          drv = crate;
+        };
+        devShell = pkgs.mkShell {
+          inputsFrom = builtins.attrValues self.checks;
+          nativeBuildInputs = [rustToolchain fenixPkgs.rust-analyzer buildDeps];
+        };
+      }
+    );
+}
